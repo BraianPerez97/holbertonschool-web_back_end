b4a6e99c78ea8f8142c3d44b961fccffc7d18a6a
BraianPerez97 <3603@holbertonstudents.com>
2023-08-28 17:52:13 -0400

    fixing documentation on floor

--- python_variable_annotations/2-floor.py
+++ python_variable_annotations/2-floor.py
@@ -3,12 +3,5 @@
 
 
 def floor(n: float) -> int:
-    """returns the floor of a float.
-
-    Args:
-        n (float): the float number to find the floor of
-
-    Returns:
-        int: the floor of the float n, the largest integer <= n.
-    """
+    """returns the floor of a float."""
     return int(n)
