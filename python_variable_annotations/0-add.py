86d9fa45c602e09877e99cc9ec93d00ac1a670b3
BraianPerez97 <3603@holbertonstudents.com>
2023-08-28 15:57:48 -0400

    adding excercise 0 and 1

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Function that add two floats"""
+from typing import Tuple
+
+
+def add(a: float, b: float) -> float:
+    return a + b
---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""function to conctenate two strings"""
+
+from typing import Tuple
+
+
+def concat(str1: str, str2: str) -> str:
+    return str1 + str2
