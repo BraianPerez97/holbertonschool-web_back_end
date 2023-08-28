8e7f5719df1b735d410c7ed2928b4ebd1e08b079
BraianPerez97 <3603@holbertonstudents.com>
2023-08-28 17:47:35 -0400

    rest of files documented

---
+++ python_variable_annotations/9-element_length.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Element length, takes a list as an argument"""
+
+
+from typing import List, Iterable, Sequence, Tuple
+
+
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """yesss it returns the juice"""
+    return [(i, len(i)) for i in lst]
