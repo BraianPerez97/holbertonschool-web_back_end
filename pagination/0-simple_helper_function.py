bf9b85e56560276d0a86898046b6398ee72f0ed4
BraianPerez97 <3603@holbertonstudents.com>
2023-09-07 18:33:02 -0400

    adding files

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -2,11 +2,11 @@
 """Pagination in python"""
 
 
-def index_range(page, page_size):
-    """Function that returns a tuple size of two containing
-    a start index and an end index corresponging
-    to the range of indexes to return in a list of 
-    pagination parameters"""
-    start_index = (page - 1) * page_size
-    end_index = start_index + page_size
-    return start_index, end_index
+def index_range(self, page: int, page_size: int) -> tuple:
+        """
+        Returns tuple of start and end indexes 
+        for given page and page size.
+        """
+        start_index = (page - 1) * page_size
+        end_index = start_index + page_size
+        return start_index, end_index
