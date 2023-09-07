5e629dab3f34b5b46b14f18b1a6b484fc5cc94b4
BraianPerez97 <3603@holbertonstudents.com>
2023-09-07 18:21:20 -0400

    simple pagination function

---
+++ pagination/1-simple_pagination.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+"""Simple pagination"""
+import csv
+import math
+from typing import List
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """Method to find the correct indexes to 
+        paginate the dataset correctly"""
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
+        page_range: tuple = self.index_range(page, page_size)
+        data = self.dataset()
+        data = data[page_range[0]: page_range[1]]
+        return data
+
+    def index_range(page, page_size):
+        """Function that returns a tuple size of two containing
+        a start index and an end index corresponging
+        to the range of indexes to return in a list of 
+        pagination parameters"""
+        start_index = (page - 1) * page_size
+        end_index = start_index + page_size
+        return start_index, end_index
