c91f64be8445e78eda9026949a0ee395e704d421
BraianPerez97 <3603@holbertonstudents.com>
2023-09-07 18:28:50 -0400

    simple pagination function

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -4,43 +4,54 @@ import csv
 import math
 from typing import List
 
-
 class Server:
-    """Server class to paginate a database of popular baby names.
     """
+    Server class to paginate a database of popular baby names.
+    """
+
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
+        """
+        Constructor method that initializes a cached dataset
+        """
         self.__dataset = None
 
     def dataset(self) -> List[List]:
-        """Cached dataset
+        """
+        Cached dataset property that loads data from CSV
+        and stores in instance attribute.
         """
         if self.__dataset is None:
             with open(self.DATA_FILE) as f:
                 reader = csv.reader(f)
                 dataset = [row for row in reader]
-            self.__dataset = dataset[1:]
+                self.__dataset = dataset[1:]
 
         return self.__dataset
 
-    def index_range(page, page_size):
-        """Function that returns a tuple size of two containing
-        a start index and an end index corresponging
-        to the range of indexes to return in a list of 
-        pagination parameters"""
+    def index_range(self, page: int, page_size: int) -> tuple:
+        """
+        Returns tuple of start and end indexes 
+        for given page and page size.
+        """
         start_index = (page - 1) * page_size
         end_index = start_index + page_size
         return start_index, end_index
     
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        """Method to find the correct indexes to 
-        paginate the dataset correctly"""
+        """
+        Returns the requested page of data
+        using the page and page_size args.
+        Validates inputs and indexes to dataset.
+        """
         assert isinstance(page, int) and page > 0
         assert isinstance(page_size, int) and page_size > 0
-        page_range: tuple = self.index_range(page, page_size)
-        data = self.dataset()
-        data = data[page_range[0]: page_range[1]]
-        return data
 
-    
+        page_range = self.index_range(page, page_size)
+        data = self.dataset()
+
+        if page_range[0] >= len(data):
+            return []
+        else:
+            return data[page_range[0]: page_range[1]]
