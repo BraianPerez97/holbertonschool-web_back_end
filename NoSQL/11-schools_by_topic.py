84db8b6088c44da263bd85be3a020b1b23ae402f
BraianPerez97 <3603@holbertonstudents.com>
2023-09-07 17:33:31 -0400

    editing documentation

--- NoSQL/11-schools_by_topic.py
+++ NoSQL/11-schools_by_topic.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
-"""This script defines a funcion that returns list of
+"""This script defines a funcion 
+that returns list of
  schools thath hace a specific subject
 """
 import pymongo
 
 
 def schools_by_topics(mongo_collection, topic):
-    """Returns list of dictionaries representing schools that have a specific topic
+    """Returns list of dictionaries representing schools 
+    that have a specific topic
     """
     schools = mongo_collection.find({"topics": {"$in": [topic]}})
     return schools
