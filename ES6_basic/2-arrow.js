4228ea560bfe1529f151c53a5970c6f550ec3e35
BraianPerez97 <3603@holbertonstudents.com>
2023-09-13 11:31:12 -0400

    adding arrows.js

---
+++ ES6_basic/2-arrow.js
@@ -0,0 +1,17 @@
+export default function getNeighborhoodsList() {
+
+    this.sanFrancisoNeighborhood = ['SOMA', 'Union Square']:
+
+    thisadd.Neighborhood = (newNeighborhood) =>{
+        this.sanFranciscoNeighborhood.push(newNeighborhood);
+        return this.sanFranciscpNeighborhood;
+    };
+}
+
+import getNeighborhoodsList from './2-arrow.js';
+
+const neighborhoodsList = new getNeighborhoodsList();
+
+const res = neighborhoodList.addNeighborhood('Noe Valley');
+
+console.log(res);
