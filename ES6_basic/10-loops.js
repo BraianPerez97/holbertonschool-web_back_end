3ac48d16eace312050a57e94dcb3137be359798b
BraianPerez97 <3603@holbertonstudents.com>
2023-09-14 14:53:13 -0400

    coding style

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,8 +1,6 @@
 export default function appendToEachArrayValue(array, appendString) {
-    const final = [];
-    for (const value of array) {
-      final.push(appendString + value);
-    }
-  
-    return final;
+  for (let value of array) {
+    value = appendString + value;
   }
+  return array;
+}
