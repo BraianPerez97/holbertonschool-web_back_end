f7ff3c893bfd4fb2d36c35fa7bde72cf0e96ba5b
BraianPerez97 <3603@holbertonstudents.com>
2023-09-14 15:00:22 -0400

    coding style

--- ES6_basic/101-iterateThroughObject.js
+++ ES6_basic/101-iterateThroughObject.js
@@ -1,13 +1,7 @@
 export default function iterateThroughObject(reportWithIterator) {
-    let employees = '';
-  
-    for (const [i, value] of reportWithIterator.entries()) {
-      if (i !== reportWithIterator.length - 1) {
-        employees += `${value} | `;
-      } else {
-        employees += value;
-      }
-    }
-  
-    return employees;
+  const employees = [];
+  for (const employee of reportWithIterator) {
+    employees.push(employee);
   }
+  return employees.join(' | ');
+}
