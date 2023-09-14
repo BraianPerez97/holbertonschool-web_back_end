c1a0a62b084b11046cb0c90fb14e8a5e4d8e9fb1
BraianPerez97 <3603@holbertonstudents.com>
2023-09-14 14:58:01 -0400

    coding style

--- ES6_basic/11-createEmployeesObject.js
+++ ES6_basic/11-createEmployeesObject.js
@@ -1,15 +1,3 @@
-export default function createIteratorObject(report) {
-  const employees = Object.values(report.allEmployees).flat();
-  
-  let index = 0;
-
-  return {
-    next() {
-      if (index < employees.length) {
-        return { value: employees[index++], done: false };
-      } else {
-        return { done: true };
-      }
-    },
-  };
-}
+export default function createEmployeesObject(departmentName, employees) {
+    return { [departmentName]: employees };
+  }
