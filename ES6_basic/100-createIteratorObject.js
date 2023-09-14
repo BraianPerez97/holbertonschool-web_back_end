69ea72832e25ba32b9f0346eeaf33e6beaa97558
BraianPerez97 <3603@holbertonstudents.com>
2023-09-14 15:04:02 -0400

    coding style

--- ES6_basic/100-createIteratorObject.js
+++ ES6_basic/100-createIteratorObject.js
@@ -1,27 +1,7 @@
-export default function createIteratorObject(report) {
-  const allEmployees = report.allEmployees;
-  const departments = Object.keys(allEmployees);
-  let departmentIndex = 0;
-  let employeeIndex = 0;
-
-  return {
-    next() {
-      if (departmentIndex < departments.length) {
-        const department = departments[departmentIndex];
-        const employeesInDepartment = allEmployees[department];
-
-        if (employeeIndex < employeesInDepartment.length) {
-          const employee = employeesInDepartment[employeeIndex];
-          employeeIndex++;
-          return { value: employee, done: false };
-        } else {
-          departmentIndex++;
-          employeeIndex = 0;
-          return this.next();
-        }
-      } else {
-        return { done: true };
-      }
-    },
-  };
+export default function createIteratorObject(Report) {
+  let results = [];
+  for (const value of Object.values(Report.allEmployees)) {
+      results = [...results, ...value];
+  }
+  return results
 }
