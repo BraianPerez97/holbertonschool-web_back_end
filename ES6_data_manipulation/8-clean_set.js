a51195e664cb2e2281358d72ad6fe9c78730fa21
BraianPerez97 <3603@holbertonstudents.com>
2023-10-13 19:10:00 -0400

    clean set

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,7 +1,14 @@
-// 8-clean_set.js
-const cleanSet = (set, startString) => [...set]
-  .filter((value) => value.startsWith(startString))
-  .map((value) => value.slice(startString.length))
-  .join('-');
-
-export default cleanSet;
+export default function cleanSet(_set, startString) {
+    if (typeof _set !== 'object' || typeof startString !== 'string' || startString.length === 0) {
+      return '';
+    }
+  
+    const string = [];
+  
+    for (const item of _set) {
+      if (item && item.startsWith(startString)) {
+        string.push(item.slice(startString.length)).join('-');
+      }
+    }
+    return string;
+  }
